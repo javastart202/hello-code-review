f527603b1d22e71ba9b54f2b62f5b4213531fc10
Pawel Koper <pawel.koper.javastart@gmail.com>
2020-07-27 18:28:14 +0200

    use Financial Office

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
       System.out.println("Hello from github");
+      Employee [] employees = {new Employee("a", "b", 2345)};
+      FinanceOffice.getAllSalaries(employees);
    }
 }
