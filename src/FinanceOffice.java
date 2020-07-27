44e37cc8007afd8e4decac7c5a74a5a79b3f26d4
Pawel Koper <pawel.koper.javastart@gmail.com>
2020-07-27 19:02:47 +0200

    fori

--- src/FinanceOffice.java
+++ src/FinanceOffice.java
@@ -4,8 +4,8 @@ public class FinanceOffice {
 
    public static double getAllSalaries(List<Employee> employees) {
       double salarySum = 0;
-      for (Employee employee : employees) {
-         salarySum += employee.getSalary();
+      for (int i = 0; i < employees.size(); i++) {
+         employees.get(i);
       }
       return salarySum;
    }
